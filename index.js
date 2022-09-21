40fe41010aa574ffe9576117abb76d74b3e43a7f
RamS24 <AlSaadi.Rami24@gmail.com>
2022-09-21 16:17:44 +0200

    final solution

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const addition = (document.querySelectorAll("input[name = 'addition']"));
-const subtraction = (document.querySelectorAll(".subtraction .in"));
-const multiplication = (document.querySelectorAll(".multiplication .in"));
-const division = (document.querySelectorAll(".division .in"));
-const modulo = (document.querySelectorAll(".modulo .in"));
+document.querySelectorAll('.in').forEach(element => element.addEventListener('input', (e) => updateOut(e)));
 
 function calculate(op, num1, num2) {
     num1 = +num1;
@@ -22,13 +18,6 @@ function calculate(op, num1, num2) {
             return -Infinity;
     }
 }
-
-// const out = document.querySelectorAll('.out');
-
-document.querySelectorAll('.in').forEach(element => element.addEventListener('input', (e) => updateOut(e)));
-
 function updateOut(e) {
     e.target.parentElement.lastElementChild.value = calculate(e.target.parentElement.className, e.target.parentElement.firstElementChild.value, e.target.parentElement.firstElementChild.nextElementSibling.value);
 }
-
-// updateOut(document.querySelector('.subtraction .in').parentElement.lastElementChild.value = calculate(parentElement.className,));
